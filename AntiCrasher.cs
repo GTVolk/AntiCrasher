6a6fc8cd9a2008d22beb79ae9b9db5a8dc0b71b2
Max Vorobyev <gtvolk31@yandex.ru>
2016-06-10 13:47:37 +0300

    AntiCrasher init

---
+++ AntiCrasher.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AntiCrasher {
+    class AntiCrasher {
+        // Constants of result
+        public const int SUCCESS = 0;
+        public const int NO_ARGUMENTS = 1;
+        public const int PARAM_IS_EMPTY = 2;
+        public const int NOT_VALID_STRUCT = 3;
+        public const int NOT_VALID_FILE_PATH = 4;
+        public const int NOT_VALID_EXEC_EXT = 5;
+        public const int NO_EXIST_EXEC_FILE = 6;
+        public const int EXCEPTION_FAIL = 10;
+
+        // Public properties
+        private string _title;
+        private string _target;
+        private bool _visible;
+        private string _exec;
+        private List<string> _params;
+
+        // Public readonly properties
+        private string[] _validExt;
+
+        // Private properties
+        private Process _p;
+
+        /// <summary>
+        ///  Constructor
+        /// </summary>
+        public AntiCrasher() {
+            this._title = "";
+            this._target = "";
+            this._visible = true;
+            this._exec = "";
+            this._params = new List<string>();
+            this._validExt = new string[] { ".bat", ".exe", ".cmd" , ".ps1" };
+        }
+
+        /// <summary>
+        /// Window title
+        /// </summary>
+        public string Title {
+            get { return this._title; }
+            set { this._title = value; }
+        }
+
+        /// <summary>
+        /// Target name
+        /// </summary>
+        public string Target {
+            get { return this._target; }
+            set { this._target = value; }
+        }
+
+        /// <summary>
+        /// Is executed process window visible
+        /// </summary>
+        public bool isVisible {
+            get { return this._visible; }
+            set { this._visible = value; }
+        }
+
+        /// <summary>
+        /// Exec file path
+        /// </summary>
+        public string ExecFile {
+            get { return this._exec; }
+            set { this._exec = value; }
+        }
+
+        /// <summary>
+        /// Exec file params
+        /// </summary>
+        public List<string> ExecParams {
+            get { return this._params; }
+            set { this._params = value; }
+        }
+
+        /// <summary>
+        /// Valid exec file extensions
+        /// </summary>
+        public string[] ValidExtensions {
+            get { return this._validExt; }
+        }
+
+        /// <summary>
+        /// Parse arguments array into class variables
+        /// </summary>
+        /// <param name="arguments">Arguments array</param>
+        /// <returns>Code of success</returns>
+        public int parseArguments(string[] arguments) {
+            if (arguments.Length < 1)
+                return NO_ARGUMENTS;
+
+            for(int i = 0; i < arguments.Length; i++) {
+                string a = arguments[i];
+
+                if (!String.IsNullOrEmpty(a)) {
+                    if (a == "-title") {
+                        i++;
+                        if (arguments.Length > i + 1) {
+                            this.Title = arguments[i];
+                            continue;
+                        } else {
+                            return NOT_VALID_STRUCT;
+                        }
+                    }
+                    if (a == "-target") {
+                        i++;
+                        if (arguments.Length > i + 1) {
+                            this.Target = arguments[i];
+                            continue;
+                        } else {
+                            return NOT_VALID_STRUCT;
+                        }
+                    }
+                    if (a == "-hidden") {
+                        this.isVisible = false;
+                        continue;
+                    }
+                    string ext = Path.GetExtension(a);
+                    if (!String.IsNullOrEmpty(ext)) {
+                        if (this.ValidExtensions.Contains(ext)) {
+                            if (File.Exists(a)) {
+                                this.ExecFile = a;
+                                for(int k = i++; k < arguments.Length; k++) {
+                                    this.ExecParams.Add(arguments[k]);
+                                }
+                                return SUCCESS;
+                            } else {
+                                return NO_EXIST_EXEC_FILE;
+                            }
+                        } else {
+                            return NOT_VALID_EXEC_EXT;
+                        }
+                    } else {
+                        return NOT_VALID_FILE_PATH;
+                    }
+                } else {
+                    return PARAM_IS_EMPTY;
+                }
+            }
+            return NOT_VALID_STRUCT;
+        }
+
+        /// <summary>
+        /// Runs desired process and restarting it redgarding exit code
+        /// </summary>
+        /// <param name="isConsole">Is method can write to console</param>
+        /// <returns>Code of success</returns>
+        public int runProcess(bool isConsole = false) {
+            do {
+                if (isConsole) {
+                    if (!String.IsNullOrEmpty(this.Target)) {
+                        Console.WriteLine(DateTime.Now + " " + this.Target +" is started");
+                    } else {
+                        Console.WriteLine(DateTime.Now + " " + this.ExecFile +" is started");
+                    }
+                }
+
+                //Setup the Process with the ProcessStartInfo class
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = this.ExecFile;
+                startInfo.Arguments = String.Join(" ", this.ExecParams.ToArray());
+                if (!this.isVisible) {
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                }
+
+                try {
+                    this._p = Process.Start(startInfo);
+                    this._p.WaitForExit();
+                } catch(Exception e) {
+                    if (isConsole) {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                    return EXCEPTION_FAIL;
+                }
+                if (isConsole) {
+                    if (this._p.ExitCode > 0) {
+                        if (!String.IsNullOrEmpty(this.Target)) {
+                            Console.WriteLine(DateTime.Now + " " + this.Target + " is closed with error code " + this._p.ExitCode + ". Restarting...");
+                        } else {
+                            Console.WriteLine(DateTime.Now + " " + this.ExecFile + " is closed with error code " + this._p.ExitCode + ". Restarting...");
+                        }
+                       
+                    } else {
+                        if (!String.IsNullOrEmpty(this.Target)) {
+                            Console.WriteLine(DateTime.Now + " " + this.Target + " is closed. Exiting...");
+                        } else {
+                            Console.WriteLine(DateTime.Now + " " + this.ExecFile + " is closed. Exiting...");
+                        }
+                        
+                    }
+                }
+                Thread.Sleep(3000);     // Wait 3 seconds before restart!
+            }
+            while (this._p.ExitCode > 0);
+            return SUCCESS;
+        }
+
+        public void cleanProcess() {
+            if (this._p != null) {
+                if (!this._p.HasExited) {
+                    this._p.Kill();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Destructor, kills process if it still running in background
+        /// </summary>
+        ~AntiCrasher() {
+            this.cleanProcess();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AntiCrasher {
+    class Program {
+
+        static bool exitSystem = false;
+
+        #region Trap application termination
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+        static AntiCrasher _ac;
+
+        enum CtrlType {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        private static bool Handler(CtrlType sig) {
+            Console.WriteLine("Exiting system due to external CTRL-C, or process kill, or shutdown");
+
+
+            _ac.cleanProcess();
+
+            //shutdown right away so there are no lingering threads
+            Environment.Exit(-1);
+
+            return true;
+        }
+        #endregion
+
+        public void Start() {
+            // start a thread and start doing some processing
+            Console.WriteLine("Thread started, processing..");
+        }
+
+        static int Main(string[] args) {
+            // Create class instance
+            _ac = new AntiCrasher();
+
+            // Some biolerplate to react to close window event, CTRL-C, kill, etc
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+
+            // Parsing arguments
+            int errNo = _ac.parseArguments(args);
+            // Check success
+            if (errNo != AntiCrasher.SUCCESS) {
+                Console.WriteLine("Usage error! Valid usage: anticrasher.exe [-title Window title] [-target Target description] protecting_file [protecting_file_params]");
+                return errNo;
+            }
+            // Check and set title
+            if (!String.IsNullOrEmpty(_ac.Title)) {
+                Console.Title = "Anti-crash " + _ac.Title;
+            } else {
+                Console.Title = "Anti-crasher";
+            }
+            // Check and set target
+            if (!String.IsNullOrEmpty(_ac.Target)) {
+                Console.WriteLine("Protecting " + _ac.Target + " from crashes...");
+            } else {
+                Console.WriteLine("Protecting " + _ac.ExecFile + " from crashes...");
+            }
+            // Anticrash process
+            return _ac.runProcess(true);
+        }
+    }
+}
